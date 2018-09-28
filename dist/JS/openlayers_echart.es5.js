e7655c02e52524e0eac74658df535da861eed3b6
a_small_tree_of_joy <bingqian_wang@qq.com>
2018-09-28 10:55:37 +0800

    更新了ES5下的资源

--- dist/JS/openlayers_echart.es5.js
+++ dist/JS/openlayers_echart.es5.js
@@ -1,5 +1,9 @@
 'use strict';
 
+Object.defineProperty(exports, "__esModule", {
+	value: true
+});
+
 var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
 
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
@@ -58,7 +62,7 @@ var ADLayer = function () {
 	}, {
 		key: '_bindEvent',
 		value: function _bindEvent() {
-			this._map.on('pointerdrag', this._moveHandler("onMoveEnd"));
+			this._map.on('postrender', this._moveHandler("onMoveEnd"));
 			this._map.getView().on('change:resolution', this._moveHandler("onMoveEnd"));
 		}
 	}, {
@@ -365,5 +369,4 @@ ADLayer.Element = {
 	}
 };
 
-// window["ADLayer"] = ADLayer;
-// export default ADLayer;
+exports.default = ADLayer;
